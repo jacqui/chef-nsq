5a3efd8d8a5a677733ddaf422f5bcae97546d21a
Mike Dewar <mikedewar@gmail.com>
2014-02-07 15:35:51 -0500

    Update nsqadmin.rb

--- attributes/nsqadmin.rb
+++ attributes/nsqadmin.rb
@@ -1,12 +1,12 @@
 # https://github.com/bitly/nsq/tree/master/nsqadmin
 
 default["nsq"]["nsqadmin"]["graphite_url"]                  = ""
-default["nsq"]["nsqadmin"]["proxy_graphite"]                = true
+default["nsq"]["nsqadmin"]["proxy_graphite"]                = false
 default["nsq"]["nsqadmin"]["http_address"]                  = "0.0.0.0:4171"
 default["nsq"]["nsqadmin"]["notification_http_endpoint"]    = nil
 default["nsq"]["nsqadmin"]["nsqd_http_address"]             = []
 default["nsq"]["nsqadmin"]["nsqd_template_dir"]             = "/usr/local/share/nsq/templates"
-default["nsq"]["nsqadmin"]["use_statsd_prefixes"]           = true
+default["nsq"]["nsqadmin"]["use_statsd_prefixes"]           = false
 default["nsq"]["nsqadmin"]["verbose"]                       = false
 default["nsq"]["nsqadmin"]["statsd_interval"]               = "30s"
 
